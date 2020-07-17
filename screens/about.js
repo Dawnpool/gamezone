52613261a9e60dd297f26d86417e0c5082dff8f3
Yeo Meong Choi <57010815+Dawnpool@users.noreply.github.com>
2020-07-18 01:36:45 +0900

    add more about

--- screens/about.js
+++ screens/about.js
@@ -5,7 +5,7 @@ import { globalStyles } from '../styles/global';
 export default function About() {
     return (
         <View style={globalStyles.container}>
-            <Text>스위치 게임 리뷰 어플입니다.</Text>
+            <Text>게임 리뷰 어플입니다. 리뷰를 남겨주세요.</Text>
         </View>
     );
 }
